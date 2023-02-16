51f1e0bbd6b7e8d7a203ba77f9a16c351affb97a
paoloricciuti <ricciutipaolo@gmail.com>
2023-02-16 16:17:13 +0100

    1 file modified

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,7 +2,7 @@ import { ServerLoad } from '@sveltejs/kit';
 let num = 0;
 export const load: ServerLoad = ({ setHeaders }) => {
 	setHeaders({
-		'Cache-Control': 's-maxage=60, stale-while-revalidate=15'
+		'Cache-Control': 's-maxage=10, stale-while-revalidate=15'
 	});
 	return {
 		num: num++
